967b2ef28cd767a567f072ecb2b380558d550905
deepakzpandey <deepakzpandey3@gmail.com>
2026-03-27 23:10:14 +0530

    fixed caching vulnerability for test series.

--- backend/src/routes/testRoutes.js
+++ backend/src/routes/testRoutes.js
@@ -24,7 +24,18 @@ router.get("/attempts/mine", authenticate, requireRole("student"), checkCache(re
 router.get("/attempts/:attemptId/result", authenticate, requireAnyRole("admin", "faculty", "student"), checkCache(req => `attempts:${req.params.attemptId}:result`, 3600), getAttemptResult);
 router.get("/attempts/:attemptId/questions/:questionId/explanation", authenticate, requireAnyRole("admin", "faculty", "student"), checkCache(req => `attempts:${req.params.attemptId}:expl:${req.params.questionId}`, 3600), getAttemptQuestionExplanation);
 router.patch("/attempts/:attemptId/progress", authenticate, requireRole("student"), saveMyAttemptProgress); // Exclude from invalidation due to high frequency, TTL expires.
-router.post("/attempts/:attemptId/submit", authenticate, requireRole("student"), invalidateCache(req => [`student:${req.user?.sub}:test_attempts`, `attempts:${req.params.attemptId}:*`, `test:${req.body?.testId}:analysis`]), submitMyAttempt);
+router.post(
+    "/attempts/:attemptId/submit",
+    authenticate,
+    requireRole("student"),
+    invalidateCache((req) => [
+        `student:${req.user?.sub}:test_attempts`,
+        `attempts:${req.params.attemptId}:*`,
+        `student:${req.user?.sub}:test:*:summary`,
+        "test:*:analysis",
+    ]),
+    submitMyAttempt
+);
 router.get("/:id/attempts/analysis", authenticate, requireAnyRole("admin", "faculty"), checkCache(req => `test:${req.params.id}:analysis`, 600), getTestAnalysis);
 router.get("/:id/attempts", authenticate, requireRole("student"), checkCache(req => `student:${req.user?.sub}:test:${req.params.id}:summary`, 600), getMyTestAttemptSummary);
 router.post("/:id/attempts/start", authenticate, requireRole("student"), invalidateCache(req => [`student:${req.user?.sub}:*`]), startMyTestAttempt);
